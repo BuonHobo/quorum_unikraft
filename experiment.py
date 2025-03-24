7e5cbbbaa0a5767fa0ac0b6671821d0db9039608
buonhobo <bnnalx@gmail.com>
2025-03-24 21:58:29 +0100

    Refactor deployment configuration by moving JSON file to root directory and updating file references

--- experiment.py
+++ experiment.py
@@ -3,7 +3,7 @@ import json
 from provinew.deployment.Deployment import Deployment
 import asyncio
 
-with open("provinew/deployment.json") as f:
+with open("deployment.json") as f:
     data = json.load(f)
     dep = Deployment(data)
     asyncio.run(dep.quorum.deploy())
